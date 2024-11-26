fa5e883cc965a10aa960c0bc706bd8b96391857d
KyryloTsekov <kir.1989@gmail.com>
2024-11-26 13:19:56 +0100

    Homework 01

---
+++ src/main/java/homework_01/Main.java
@@ -0,0 +1,13 @@
+package homework_01;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Домашнее задание: посчитать 1 + 1
+        int a = 1;
+        int b = 1;
+        int result = a + b;
+        System.out.println(result);
+    }
+}
